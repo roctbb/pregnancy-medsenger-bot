f6d9784933121e5494c3e5de1124a5b27e5bfcce
roctbb <roctbb@gmail.com>
2020-12-15 17:16:02 +0300

    mvp

--- pregnancy-bot.py
+++ pregnancy-bot.py
@@ -7,6 +7,7 @@ import threading
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 import agents_api
+import os, sys
 
 app = Flask(__name__)
 db_string = "postgres://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
@@ -315,6 +316,9 @@ def settings():
             return "<strong>Ошибка. Контракт не найден.</strong> Попробуйте отключить и снова подключить интеллектуальный агент к каналу консультирвоания.  Если это не сработает, свяжитесь с технической поддержкой."
 
     except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
         print(e)
         return "error"
 
