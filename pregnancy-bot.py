317bff930f12aca72943de4e1153518fd232f049
roctbb <roctbb@gmail.com>
2020-12-15 17:20:18 +0300

    mvp

--- pregnancy-bot.py
+++ pregnancy-bot.py
@@ -113,7 +113,10 @@ class Contract(db.Model):
 
             db.session.commit()
         except Exception as e:
-            pass
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(e)
 
     def check_measurements(self):
 
