fa60c42041b6dd980fe3c80b28f3c239ce196adf
roctbb <roctbb@gmail.com>
2022-03-10 18:10:15 +0300

    Update pregnancy_bot.py

--- pregnancy_bot.py
+++ pregnancy_bot.py
@@ -10,7 +10,7 @@ import agents_api
 import os, sys
 
 app = Flask(__name__)
-db_string = "postgres://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
+db_string = "postgresql://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 db = SQLAlchemy(app)
 
